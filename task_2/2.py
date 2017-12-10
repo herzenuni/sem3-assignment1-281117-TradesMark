23496f4b0d674fa369292f83985bc19f02b11ce3
Anton Galkin <artemis9898@mail.ru>
2017-12-10 15:43:04 +0300

    Update 2.py

--- task_2/2.py
+++ task_2/2.py
@@ -1,1 +1,18 @@
+#
 
+def sum_number_in_2(number):
+	number = str(number)
+	sum = 0
+	for ch in number:
+		sum = sum + int(ch) ** 2
+	return sum
+
+def fun():
+	n = int(input("n = "))
+	print("{}значные числа, сумма квадратов цифр которых делится на 17:". format(n))
+	for i in range(10 ** (n - 1), 10 ** n):
+		if sum_number_in_2(i) % 17 == 0:
+			print(i, end = ', ')
+	print()
+
+	fun()
